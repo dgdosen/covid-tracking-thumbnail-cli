a380211b1004abe214e23e815f0c3a6676fde439
Daniel Dosen <dgdosen@gmail.com>
2020-05-14 06:00:11 -0700

    wip: updating capture to focus on daily update

--- index.js
+++ index.js
@@ -6,9 +6,39 @@ const getScreenshot = async (url) => {
   await page.goto(`https://covid.agidevelopment.com`, {
     waitUntil: 'networkidle0',
   });
-  await page.screenshot({
-    path: `screenshot.png`,
-    fullPage: true,
+  // await page.screenshot({
+  //   path: `screenshot.png`,
+  //   fullPage: true,
+  // });
+
+  async function screenshotDOMElement(opts = {}) {
+    const padding = 'padding' in opts ? opts.padding : 0;
+    const path = 'path' in opts ? opts.path : null;
+    const selector = opts.selector;
+    if (!selector) throw Error('Please provide a selector.');
+    const rect = await page.evaluate((selector) => {
+      const element = document.querySelector(selector);
+      if (!element) return null;
+      const { x, y, width, height } = element.getBoundingClientRect();
+      return { left: x, top: y, width, height, id: element.id };
+    }, selector);
+    if (!rect)
+      throw Error(`Could not find element that matches selector: ${selector}.`);
+    return await page.screenshot({
+      path,
+      clip: {
+        x: rect.left - padding,
+        y: rect.top - padding,
+        width: rect.width + padding * 2,
+        height: rect.height + padding * 2,
+      },
+    });
+  }
+
+  await screenshotDOMElement({
+    path: 'screenshot.png',
+    selector: '#covid-daily-linechart',
+    padding: 16,
   });
   await browser.close();
 };
